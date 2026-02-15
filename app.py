435da8b03697f24a973affae22b910129a1b0f9a
ChandanaBalamurugan <chandanabalamurugan05@gmail.com>
2026-02-16 05:05:49 +0530

    Added Streamlit MVP

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🎓 Fraud Proof Attendance System")
+st.write("AI-Based Face Recognition Attendance")
+
+if st.button("Mark Attendance"):
+    st.success("Attendance Marked Successfully!")
